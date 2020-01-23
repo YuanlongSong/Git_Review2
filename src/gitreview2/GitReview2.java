5e33269ce20f55faf71beba7d95133446d675359
YuanlongSong <songyua@sheridancollege.ca>
2020-01-23 14:43:41 -0500

    First commit this project

---
+++ src/gitreview2/GitReview2.java
@@ -0,0 +1,16 @@
+package gitreview2;
+
+/**
+ *This class +++Insert Description here+++
+ * @author Yuanlong Song
+ */
+public class GitReview2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
